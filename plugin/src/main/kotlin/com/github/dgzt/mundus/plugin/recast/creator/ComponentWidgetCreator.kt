62261d5ab88175d055f88e58330b28f74fd1d359
Zsuró Tibor <zsurotibor@gmail.com>
2024-11-25 16:50:36 +0100

    Generating navmesh in thread.

--- plugin/src/main/kotlin/com/github/dgzt/mundus/plugin/recast/creator/ComponentWidgetCreator.kt
+++ plugin/src/main/kotlin/com/github/dgzt/mundus/plugin/recast/creator/ComponentWidgetCreator.kt
@@ -10,42 +10,69 @@ import com.github.jamestkhan.recast.utils.NavMeshIO
 import com.mbrlabs.mundus.commons.scene3d.components.Component
 import com.mbrlabs.mundus.commons.scene3d.components.TerrainComponent
 import com.mbrlabs.mundus.pluginapi.ui.RootWidget
+import com.mbrlabs.mundus.pluginapi.ui.RootWidgetCell
 import com.mbrlabs.mundus.pluginapi.ui.WidgetAlign
 
 object ComponentWidgetCreator {
 
-    fun setup(component: RecastNavMeshComponent, rootWidget: RootWidget) {
-        rootWidget.addTextButton("Generate NavMesh") {
-            Gdx.app.log("", "Click")
+    private const val TILE_SIZE_X_DEFAULT_VALUE = 128
+    private const val TILE_SIZE_Y_DEFAULT_VALUE = 128
 
+    fun setup(component: RecastNavMeshComponent, rootWidget: RootWidget) {
+        var newNavMeshWidgetRootCell: RootWidgetCell? = null
+        rootWidget.addTextButton("New NavMesh") {
+            val newNavMeshWidgetRoot = newNavMeshWidgetRootCell!!.rootWidget
+            newNavMeshWidgetRoot.clearWidgets()
+            addNewNavMeshWidget(component, newNavMeshWidgetRoot)
+        }.setAlign(WidgetAlign.CENTER)
+        rootWidget.addRow()
+        newNavMeshWidgetRootCell = rootWidget.addEmptyWidget()
+    }
+
+    private fun addNewNavMeshWidget(component: RecastNavMeshComponent, rootWidget: RootWidget) {
+        var tileSizeX = TILE_SIZE_X_DEFAULT_VALUE
+        var tileSizeY = TILE_SIZE_Y_DEFAULT_VALUE
+
+        rootWidget.addSpinner("Tile Size X", 8, Int.MAX_VALUE, tileSizeX) { tileSizeX = it }.setAlign(WidgetAlign.LEFT)
+        rootWidget.addRow()
+        rootWidget.addSpinner("Tile Size Y", 8, Int.MAX_VALUE, tileSizeY) { tileSizeY = it }.setAlign(WidgetAlign.LEFT)
+        rootWidget.addRow()
+        rootWidget.addTextButton("Generate NavMesh") {
             val terrainComponent = findTerrainComponent(component)
             if (terrainComponent == null) {
                 // TODO text
                 return@addTextButton
             }
 
-            // TODO generate navmesh in thread
+            Thread {
+                val settings = NavMeshGenSettings.Builder.SettingsBuilder()
+                    .useTiles(true)
+                    .tileSizeX(tileSizeX)
+                    .tileSizeZ(tileSizeY)
+                    .build()
 
-            // TODO Set parameters on UI
-            val settings = NavMeshGenSettings.Builder.SettingsBuilder()
-                .useTiles(true)
-                .tileSizeX(128)
-                .tileSizeZ(128)
-                .build()
+                val navMeshGenerator = NavMeshGenerator(terrainComponent.modelInstance)
+                Gdx.app.log("Recast NavMesh Plugin", "Generating...")
+                val navMeshData = navMeshGenerator.build(settings)
+                Gdx.app.log("Recast NavMesh Plugin", "Generated.")
 
-            val navMeshGenerator = NavMeshGenerator(terrainComponent.modelInstance)
-            val navMeshData = navMeshGenerator.build(settings)
+                val tmpDir = System.getProperty("java.io.tmpdir")
+                val tmpFile = FileHandle("$tmpDir/${terrainComponent.gameObject.name}.navmesh")
+                NavMeshIO.save(navMeshData.navMesh, tmpFile)
 
-            val tmpDir = System.getProperty("java.io.tmpdir")
-            val tmpFile = FileHandle("$tmpDir/${terrainComponent.gameObject.name}.navmesh")
-            NavMeshIO.save(navMeshData.navMesh, tmpFile)
+                Gdx.app.postRunnable {
+                    // TODO handle asset already exception
+                    val asset = PropertyManager.assetManager.createNewAsset(tmpFile)
+                    component.asset = asset
 
-            // TODO handle asset already exception
-            var asset = PropertyManager.assetManager.createNewAsset(tmpFile)
-            component.asset = asset
+                    tmpFile.delete()
+                }
 
-            tmpFile.delete()
-        }.setAlign(WidgetAlign.CENTER)
+            }.start()
+
+
+        }
+
     }
 
     private fun findTerrainComponent(component: Component): TerrainComponent? {
